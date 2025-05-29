40e1fd8ec142638eb604dcd0c10f470bef3be90e
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2025-05-29 16:56:33 -0600

    cleanup

---
+++ src/pages/analytics.js
@@ -0,0 +1,9 @@
+export function trackFormSubmission(data) {
+  console.log('Form submission tracked:', data);
+  // Add Google Analytics tracking logic here
+}
+
+export function trackPageView() {
+  console.log('Page view tracked');
+  // Add Google Analytics tracking logic here
+}
